1f062f724a1e8228ff871376daabc5f2968ab875
wuqiyou <davidwu1221@gmail.com>
2016-07-27 15:49:19 -0400

    refactor

--- Slice.Web.Models/Shared/SocialShareBarViewModel.cs
+++ Slice.Web.Models/Shared/SocialShareBarViewModel.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Slice.Web.Models.Shared
 {
@@ -14,13 +11,6 @@ namespace Slice.Web.Models.Shared
 
     public class SocialShareButton
     {
-        public static readonly Dictionary<SocialShareButtonType, string> URL_FORMAT_DEFAULTS = new Dictionary<SocialShareButtonType, string>() {
-            // TODO: add share link formats
-            { SocialShareButtonType.Facebook, "http://facebook.com/sharer.php?u={0}" },
-            { SocialShareButtonType.Twitter, "https://twitter.com/intent/tweet?url={0}&text={1}" },
-            { SocialShareButtonType.Pinterest, "https://www.pinterest.com/pin/create/bookmarklet/?url={0}&media={2}&description={1}" }
-        };
-
         public string Key { get; set; }
         public string Name { get; set; }
         public string Url { get; set; }
@@ -28,6 +18,13 @@ namespace Slice.Web.Models.Shared
 
     public class SocialShareBarViewModel
     {
+        public static readonly Dictionary<SocialShareButtonType, string> URL_FORMAT_DEFAULTS = new Dictionary<SocialShareButtonType, string>() {
+            // TODO: add share link formats
+            { SocialShareButtonType.Facebook, "http://facebook.com/sharer.php?u={0}" },
+            { SocialShareButtonType.Twitter, "https://twitter.com/intent/tweet?url={0}&text={1}" },
+            { SocialShareButtonType.Pinterest, "https://www.pinterest.com/pin/create/bookmarklet/?url={0}&media={2}&description={1}" }
+        };
+
         public List<SocialShareButton> ShareButtons { get; set; }
 
         public SocialShareBarViewModel(string pageUrl, string shareTitle = "", string shareImageUrl = "")
@@ -44,7 +41,7 @@ namespace Slice.Web.Models.Shared
             {
                 SocialShareButton button = new SocialShareButton() { Key = icon.ToString().ToLower(), Name = icon.ToString() };
                 ShareButtons.Add(button);
-                string urlStringFormat = SocialShareButton.URL_FORMAT_DEFAULTS[icon];
+                string urlStringFormat = SocialShareBarViewModel.URL_FORMAT_DEFAULTS[icon];
                 button.Url = string.Format(urlStringFormat, pageUrl, shareTitle, shareImageUrl);
             }
         }
