8bdc88466e3f75b7762d49fd8cd87c303568cf6c
wuqiyou <davidwu1221@gmail.com>
2016-09-26 23:40:53 -0400

    add AdWidget.cs

---
+++ Slice.Web.Models/Widgets/AdWidget.cs
@@ -0,0 +1,15 @@
+﻿using Slice.Core;
+using Slice.Data;
+using SubjectEngine.Core;
+
+namespace Slice.Web.Models.Widgets
+{
+    public class AdWidget : WidgetViewModel
+    {
+        public AdType AdType { get; set; }
+
+        public override void Populate(ReferenceInfoDto referenceInfo)
+        {
+        }
+    }
+}
